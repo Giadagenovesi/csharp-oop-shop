64c6fef5adf94f12cad61cb136bed68500b91256
Giadagenovesi <giada.g@outlook.it>
2023-09-08 17:55:03 +0200

    Completato parte dell'esercizio base

---
+++ csharp-oop-shop/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        //ATTRIBUTI
+        private int code;
+        private string name;
+        private string description;
+        private float price;
+        private float iva;
+
+       
+        //PROPRIETÀ
+
+        public int Code
+        {
+            get { return code; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public float Iva
+        {
+            get { return iva; }
+            set { iva = value; }
+        }
+        public float taxedPrice
+        {
+            get { return Price + (Price /100 * iva); }
+        }
+
+        //COSTRUTTORE
+        public Prodotto(string name, float price, float iva)
+        {
+            Random random = new Random();
+            code = random.Next(00000001, 99999999);
+            Name = name;
+            Price = price;
+            Iva = iva;
+        }
+
+        //METODO
+
+        public string GenerateFullName()
+        {
+            string productFullName = name + code;
+            return productFullName;
+        }
+
+        
+
+
+
+
+
+    }
+
+ 
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Prodotto pasta = new Prodotto("Spaghetti", 1.10f, 5);
+            Console.Write($"Ti presentiamo il prodotto { pasta.Name }, ");
+            Console.Write($"codice prodotto {pasta.Code}, ");
+            Console.Write($"lo puoi trovare solo per questa settimana al magico prezzo di {pasta.taxedPrice} euro, ");
+            Console.Write($"l'iva applicata sul prodotto è del {pasta.Iva}%, ");
+            Console.Write($"il prezzo del prodotto senza iva è di {pasta.Price} euro, ");
+            
+
+
+
+
         }
     }
 }
